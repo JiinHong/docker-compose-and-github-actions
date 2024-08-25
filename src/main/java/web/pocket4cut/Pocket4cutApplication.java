9fa40ed9abbee2a899bf5c7cbaed1110ee104b27
JiinHong <com5942@seoultech.ac.kr>
2024-08-25 10:33:54 +0900

    배포

---
+++ src/main/java/web/pocket4cut/Pocket4cutApplication.java
@@ -0,0 +1,13 @@
+package web.pocket4cut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pocket4cutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pocket4cutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web/pocket4cut/Pocket4cutApplicationTests.java
@@ -0,0 +1,13 @@
+package web.pocket4cut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pocket4cutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
